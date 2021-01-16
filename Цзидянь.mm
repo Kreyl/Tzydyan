f488e6372e57c92ebf12032c90d2e7bdfd4b7984
kreyl <laystoll@yandex.ru>
2021-01-17 00:00:55 +0300

    Snake sch seems done

---
+++ Цзидянь.mm
@@ -0,0 +1,91 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Цзидянь" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1610655148385"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Браслет" POSITION="right" ID="ID_1850013914" CREATED="1610655190039" MODIFIED="1610655193226">
+<node TEXT="Радио" ID="ID_147117478" CREATED="1610655197006" MODIFIED="1610655199597">
+<node TEXT="SPI" ID="ID_672777895" CREATED="1610825833762" MODIFIED="1610825835137"/>
+</node>
+<node TEXT="Кнопка х2" ID="ID_725191720" CREATED="1610655200034" MODIFIED="1610656051924"/>
+<node TEXT="RGB диод х2" ID="ID_25989751" CREATED="1610655203393" MODIFIED="1610655213636">
+<node TEXT="Это глаза змеи" ID="ID_1751589178" CREATED="1610655410712" MODIFIED="1610655416406"/>
+</node>
+<node TEXT="Акселерометр" ID="ID_546499831" CREATED="1610655670772" MODIFIED="1610655674871">
+<node TEXT="SPI" ID="ID_684051037" CREATED="1610825837537" MODIFIED="1610825839040"/>
+</node>
+<node TEXT="Зарядка" ID="ID_1607403998" CREATED="1610655699329" MODIFIED="1610655704909"/>
+<node TEXT="Полуцилиндр, на нём змея" ID="ID_1450327932" CREATED="1610655419019" MODIFIED="1610655437757">
+<node TEXT="рядом со змеёй бугорки" ID="ID_1117620745" CREATED="1610655439655" MODIFIED="1610655451999">
+<node TEXT="Это кнопки" ID="ID_248604874" CREATED="1610655452001" MODIFIED="1610655456260"/>
+</node>
+</node>
+</node>
+<node TEXT="Кнут" POSITION="right" ID="ID_93617624" CREATED="1610655193593" MODIFIED="1610655195362">
+<node TEXT="Рукоять" ID="ID_943221745" CREATED="1610655955487" MODIFIED="1610655968731"/>
+<node TEXT="Крышка на торце, под ней выключатель и USB" ID="ID_486890995" CREATED="1610656127530" MODIFIED="1610656149590"/>
+<node TEXT="18650" OBJECT="java.lang.Long|18650" ID="ID_584226967" CREATED="1610655969009" MODIFIED="1610655972256"/>
+<node TEXT="Зарядка" ID="ID_1322127537" CREATED="1610655972971" MODIFIED="1610655981108"/>
+<node TEXT="USB" ID="ID_1396185051" CREATED="1610656187377" MODIFIED="1610656190756"/>
+<node TEXT="Акселерометр" ID="ID_1473271290" CREATED="1610655982306" MODIFIED="1610655989238"/>
+<node TEXT="Радио" ID="ID_26474911" CREATED="1610656192251" MODIFIED="1610656198634"/>
+<node TEXT="WS2812" ID="ID_1412476855" CREATED="1610656199417" MODIFIED="1610656203974"/>
+<node TEXT="Дырки для кнопки" ID="ID_1019093636" CREATED="1610656317738" MODIFIED="1610656323670"/>
+<node TEXT="Длина" ID="ID_905581991" CREATED="1610656362227" MODIFIED="1610656375779">
+<node TEXT="1.5м?" ID="ID_1797456760" CREATED="1610657423783" MODIFIED="1610657433872"/>
+</node>
+<node TEXT="Радио" ID="ID_1091214550" CREATED="1610657436178" MODIFIED="1610657438445"/>
+</node>
+</node>
+</map>
